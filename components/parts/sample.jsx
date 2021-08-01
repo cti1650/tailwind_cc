e6cebd7c73c665418a4a1c641829698423395e58
cti1650 <cti1650@gmail.com>
2021-08-02 04:23:33 +0900

    Boxレイアウト 複数パターン追加

--- components/parts/sample.jsx
+++ components/parts/sample.jsx
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import cc from "classcat";
 export const SampleBox = (props) => {
   const inputEl = useRef(null);
   const { className } = props;
@@ -6,21 +7,67 @@ export const SampleBox = (props) => {
     inputEl.current.value = className;
     inputEl.current.select();
     document.execCommand("Copy");
-    alert("コピーしました「" + inputEl.current.value + "」");
+    const comment = "コピーしました「" + inputEl.current.value + "」";
+    console.log(comment);
+    alert(comment);
   };
   return (
     <>
-      <div className="p-2">
+      <div className="p-2 h-24 w-24" title={className}>
         <div
-          className={className + " border bg-white h-20 w-20"}
+          className={cc([
+            className,
+            "border bg-white h-full w-full p-2 rounded"
+          ])}
           onClick={handleClick}
         >
-          <div className="border bg-blue-400 h-10 w-10"></div>
+          <div className="w-full h-full border bg-blue-400 rounded"></div>
         </div>
         <input
           type="text"
           defaultValue={className}
-          className="outline-none text-xs w-20"
+          className="w-0 h-0 p-0 m-0 outline-none text-xs"
+          ref={inputEl}
+        />
+      </div>
+    </>
+  );
+};
+
+export const SampleBoxs = (props) => {
+  const inputEl = useRef(null);
+  const { className } = props;
+  const handleClick = () => {
+    inputEl.current.value = className;
+    inputEl.current.select();
+    document.execCommand("Copy");
+    const comment = "コピーしました「" + inputEl.current.value + "」";
+    console.log(comment);
+    alert(comment);
+  };
+  return (
+    <>
+      <div className="p-2 h-24 w-24" title={className}>
+        <div
+          className={cc([
+            className,
+            "border bg-white h-full w-full p-2 rounded"
+          ])}
+          onClick={handleClick}
+        >
+          <div className="w-full h-full border bg-blue-400 rounded"></div>
+          <div className="w-full h-full border bg-blue-400 rounded"></div>
+          <div className="w-full h-full border bg-blue-400 rounded"></div>
+          <div className="w-full h-full border bg-blue-400 rounded"></div>
+          <div className="w-full h-full border bg-blue-400 rounded"></div>
+          <div className="w-full h-full border bg-blue-400 rounded"></div>
+          <div className="w-full h-full border bg-blue-400 rounded"></div>
+          <div className="w-full h-full border bg-blue-400 rounded"></div>
+        </div>
+        <input
+          type="text"
+          defaultValue={className}
+          className="w-0 h-0 p-0 m-0 outline-none text-xs"
           ref={inputEl}
         />
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import Nav from "../components/nav";
-import { SampleBox } from "../components/parts/sample";
+import { SampleBox, SampleBoxs } from "../components/parts/sample";
 
 const Home = () => (
   <div className="text-sans">
@@ -10,17 +10,19 @@ const Home = () => (
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
-    <div className="w-full text-center text-lg font-bold">Tailwind CC</div>
+    <div className="w-full text-center text-2xl font-bold">Tailwind CC</div>
     <div className="w-full text-gray-900 p-4">
+      <div className="text-lg font-bold">Box</div>
       <div className="flex flex-row flex-wrap justify-center items-center">
         <SampleBox className="flex justify-center items-center" />
-        <SampleBox className="flex justify-center items-center" />
-        <SampleBox className="flex justify-center items-center" />
-        <SampleBox className="flex justify-center items-center" />
-        <SampleBox className="flex justify-center items-center" />
-        <SampleBox className="flex justify-center items-center" />
-        <SampleBox className="flex justify-center items-center" />
-        <SampleBox className="flex justify-center items-center" />
+        <SampleBoxs className="flex justify-center items-center" />
+        <SampleBoxs className="flex flex-col" />
+        <SampleBoxs className="flex flex-row" />
+        <SampleBoxs className="grid grid-cols-1" />
+        <SampleBoxs className="grid grid-cols-2" />
+        <SampleBoxs className="grid grid-cols-3" />
+        <SampleBoxs className="grid grid-cols-4" />
+        <SampleBoxs className="grid grid-cols-5" />
       </div>
     </div>
   </div>

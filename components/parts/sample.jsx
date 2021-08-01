7e6752f117295610812cb305b30efb271fc929ef
cti1650 <cti1650@gmail.com>
2021-08-02 02:51:04 +0900

    sample  add

---
+++ components/parts/sample.jsx
@@ -0,0 +1,27 @@
+import { useRef } from "react";
+export const SampleBox = (props) => {
+  const inputEl = useRef(null);
+  const { className } = props;
+  const handleClick = () => {
+    inputEl.current.value = className;
+    inputEl.current.select();
+    document.execCommand("Copy");
+    alert("コピーしました「" + inputEl.current.value + "」");
+  };
+  return (
+    <>
+      <div
+        className={className + " border bg-white h-20 w-20"}
+        onClick={handleClick}
+      >
+        <div className="border bg-blue-400 h-10 w-10"></div>
+      </div>
+      <input
+        type="text"
+        defaultValue={className}
+        className="outline-none text-xs w-20"
+        ref={inputEl}
+      />
+    </>
+  );
+};

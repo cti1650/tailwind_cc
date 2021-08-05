06174b94fbf4226312844f73774c4961a563e38d
cti1650 <cti1650@gmail.com>
2021-08-05 22:06:15 +0900

    💄 popupのデザイン修正

--- components/popup/pop.jsx
+++ components/popup/pop.jsx
@@ -1,28 +1,28 @@
-import { useCallback, useEffect, useState } from "react";
-import cc from "classcat";
-import copy from "clipboard-copy";
+import { useCallback, useEffect, useState } from 'react';
+import cc from 'classcat';
+import copy from 'clipboard-copy';
 
 export const Popup = (props) => {
   const { onClose, comment } = props;
   const [open, setOpen] = useState(false);
   useEffect(() => {
-    if(comment){
+    if (comment) {
       setOpen(true);
     }
   }, [comment]);
-  useEffect(()=>{
-    if(open){
+  useEffect(() => {
+    if (open) {
       setTimeout(() => {
         setOpen(false);
-      }, 1000);
+      }, 2000);
     }
-  },[open])
+  }, [open]);
   return (
     <>
       <div
         className={cc([
-          "fixed bottom-0 right-0 py-1 px-4 border shadow-lg bg-gray-100 text-gray-800 rounded-tl duration-300",
-          {  "invisible": !open },
+          'fixed bottom-0 right-0 py-2 px-4 border shadow-lg bg-gray-100 text-gray-800 rounded-tl duration-300',
+          { invisible: !open },
         ])}
       >
         {comment}

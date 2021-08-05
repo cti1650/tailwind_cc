3fc2672558c9f279ec1cd83cf952c55949042b1c
cti1650 <cti1650@gmail.com>
2021-08-05 22:04:27 +0900

    💄 中央配置用のコンポーネントを修正

--- components/parts/sample.jsx
+++ components/parts/sample.jsx
@@ -1,19 +1,19 @@
-import { useCallback } from "react";
-import cc from "classcat";
-import copy from "clipboard-copy";
+import { useCallback } from 'react';
+import cc from 'classcat';
+import copy from 'clipboard-copy';
 
 export const SampleItem = (props) => {
   const { className, title, optionStyle, onCopy } = props;
   const handleClick = () => {
     copy(className);
-    const comment = "コピーしました「" + className + "」";
+    const comment = 'コピーしました「' + className + '」';
     onCopy(comment);
     console.log(comment);
   };
   return (
     <>
       <div
-        className={cc([className, optionStyle, "border h-full w-full rounded"])}
+        className={cc([className, optionStyle, 'border h-full w-full rounded'])}
         title={className}
         onClick={handleClick}
       >
@@ -26,14 +26,14 @@ export const SampleInlineItem = (props) => {
   const { className, title, optionStyle, onCopy } = props;
   const handleClick = () => {
     copy(className);
-    const comment = "コピーしました「" + className + "」";
+    const comment = 'コピーしました「' + className + '」';
     onCopy(comment);
     console.log(comment);
   };
   return (
     <>
       <div
-        className={cc([className, optionStyle, "border h-full w-full rounded"])}
+        className={cc([className, optionStyle, 'border h-full w-full rounded'])}
         title={className}
         onClick={handleClick}
       >
@@ -44,25 +44,29 @@ export const SampleInlineItem = (props) => {
 };
 
 export const SampleBox = (props) => {
-  const { className, dammyStyle, fitNone , onCopy } = props;
+  const { className, dammyStyle, size, onCopy } = props;
   const handleClick = () => {
     copy(className);
-    const comment = "コピーしました「" + className + "」";
+    const comment = 'コピーしました「' + className + '」';
     onCopy(comment);
     console.log(comment);
   };
   return (
     <>
-      <div className="p-2 h-24 w-24" title={className}>
+      <div className='p-2 h-24 w-24' title={className}>
         <div
           className={cc([
-            "border bg-white p-2 rounded",
-            {"h-full w-full":!fitNone},
+            'border bg-white p-2 rounded w-full h-full',
             dammyStyle ? dammyStyle : className,
           ])}
           onClick={handleClick}
         >
-          <div className="w-full h-full border bg-blue-400 rounded"></div>
+          <div
+            className={cc([
+              'border bg-blue-400 rounded',
+              { 'h-full w-full': !size, 'h-6 w-6': size === 'small' },
+            ])}
+          ></div>
         </div>
       </div>
     </>
@@ -70,32 +74,31 @@ export const SampleBox = (props) => {
 };
 
 export const SampleBoxs = (props) => {
-  const { className, dammyStyle, fitNone, onCopy } = props;
+  const { className, dammyStyle, onCopy } = props;
   const handleClick = useCallback(() => {
     copy(className);
-    const comment = "コピーしました「" + className + "」";
+    const comment = 'コピーしました「' + className + '」';
     onCopy(comment);
     console.log(comment);
   }, [className]);
   return (
     <>
-      <div className="p-2 h-24 w-24" title={className}>
+      <div className='p-2 h-24 w-24' title={className}>
         <div
           className={cc([
-            "border bg-white p-2 rounded",
-            {"h-full w-full":!fitNone},
+            'border bg-white p-2 rounded w-full h-full',
             dammyStyle ? dammyStyle : className,
           ])}
           onClick={handleClick}
         >
-          <div className="w-full h-full border bg-blue-400 rounded"></div>
-          <div className="w-full h-full border bg-blue-400 rounded"></div>
-          <div className="w-full h-full border bg-blue-400 rounded"></div>
-          <div className="w-full h-full border bg-blue-400 rounded"></div>
-          <div className="w-full h-full border bg-blue-400 rounded"></div>
-          <div className="w-full h-full border bg-blue-400 rounded"></div>
-          <div className="w-full h-full border bg-blue-400 rounded"></div>
-          <div className="w-full h-full border bg-blue-400 rounded"></div>
+          <div className='w-full h-full border bg-blue-400 rounded'></div>
+          <div className='w-full h-full border bg-blue-400 rounded'></div>
+          <div className='w-full h-full border bg-blue-400 rounded'></div>
+          <div className='w-full h-full border bg-blue-400 rounded'></div>
+          <div className='w-full h-full border bg-blue-400 rounded'></div>
+          <div className='w-full h-full border bg-blue-400 rounded'></div>
+          <div className='w-full h-full border bg-blue-400 rounded'></div>
+          <div className='w-full h-full border bg-blue-400 rounded'></div>
         </div>
       </div>
     </>
@@ -104,86 +107,86 @@ export const SampleBoxs = (props) => {
 
 export const tw_colors = () => {
   return [
-    "bg-gray-50",
-    "bg-gray-100",
-    "bg-gray-200",
-    "bg-gray-300",
-    "bg-gray-400",
-    "bg-gray-500",
-    "bg-gray-600",
-    "bg-gray-700",
-    "bg-gray-800",
-    "bg-gray-900",
-    "bg-red-50",
-    "bg-red-100",
-    "bg-red-200",
-    "bg-red-300",
-    "bg-red-400",
-    "bg-red-500",
-    "bg-red-600",
-    "bg-red-700",
-    "bg-red-800",
-    "bg-red-900",
-    "bg-yellow-50",
-    "bg-yellow-100",
-    "bg-yellow-200",
-    "bg-yellow-300",
-    "bg-yellow-400",
-    "bg-yellow-500",
-    "bg-yellow-600",
-    "bg-yellow-700",
-    "bg-yellow-800",
-    "bg-yellow-900",
-    "bg-green-50",
-    "bg-green-100",
-    "bg-green-200",
-    "bg-green-300",
-    "bg-green-400",
-    "bg-green-500",
-    "bg-green-600",
-    "bg-green-700",
-    "bg-green-800",
-    "bg-green-900",
-    "bg-blue-50",
-    "bg-blue-100",
-    "bg-blue-200",
-    "bg-blue-300",
-    "bg-blue-400",
-    "bg-blue-500",
-    "bg-blue-600",
-    "bg-blue-700",
-    "bg-blue-800",
-    "bg-blue-900",
-    "bg-indigo-50",
-    "bg-indigo-100",
-    "bg-indigo-200",
-    "bg-indigo-300",
-    "bg-indigo-400",
-    "bg-indigo-500",
-    "bg-indigo-600",
-    "bg-indigo-700",
-    "bg-indigo-800",
-    "bg-indigo-900",
-    "bg-purple-50",
-    "bg-purple-100",
-    "bg-purple-200",
-    "bg-purple-300",
-    "bg-purple-400",
-    "bg-purple-500",
-    "bg-purple-600",
-    "bg-purple-700",
-    "bg-purple-800",
-    "bg-purple-900",
-    "bg-pink-50",
-    "bg-pink-100",
-    "bg-pink-200",
-    "bg-pink-300",
-    "bg-pink-400",
-    "bg-pink-500",
-    "bg-pink-600",
-    "bg-pink-700",
-    "bg-pink-800",
-    "bg-pink-900",
+    'bg-gray-50',
+    'bg-gray-100',
+    'bg-gray-200',
+    'bg-gray-300',
+    'bg-gray-400',
+    'bg-gray-500',
+    'bg-gray-600',
+    'bg-gray-700',
+    'bg-gray-800',
+    'bg-gray-900',
+    'bg-red-50',
+    'bg-red-100',
+    'bg-red-200',
+    'bg-red-300',
+    'bg-red-400',
+    'bg-red-500',
+    'bg-red-600',
+    'bg-red-700',
+    'bg-red-800',
+    'bg-red-900',
+    'bg-yellow-50',
+    'bg-yellow-100',
+    'bg-yellow-200',
+    'bg-yellow-300',
+    'bg-yellow-400',
+    'bg-yellow-500',
+    'bg-yellow-600',
+    'bg-yellow-700',
+    'bg-yellow-800',
+    'bg-yellow-900',
+    'bg-green-50',
+    'bg-green-100',
+    'bg-green-200',
+    'bg-green-300',
+    'bg-green-400',
+    'bg-green-500',
+    'bg-green-600',
+    'bg-green-700',
+    'bg-green-800',
+    'bg-green-900',
+    'bg-blue-50',
+    'bg-blue-100',
+    'bg-blue-200',
+    'bg-blue-300',
+    'bg-blue-400',
+    'bg-blue-500',
+    'bg-blue-600',
+    'bg-blue-700',
+    'bg-blue-800',
+    'bg-blue-900',
+    'bg-indigo-50',
+    'bg-indigo-100',
+    'bg-indigo-200',
+    'bg-indigo-300',
+    'bg-indigo-400',
+    'bg-indigo-500',
+    'bg-indigo-600',
+    'bg-indigo-700',
+    'bg-indigo-800',
+    'bg-indigo-900',
+    'bg-purple-50',
+    'bg-purple-100',
+    'bg-purple-200',
+    'bg-purple-300',
+    'bg-purple-400',
+    'bg-purple-500',
+    'bg-purple-600',
+    'bg-purple-700',
+    'bg-purple-800',
+    'bg-purple-900',
+    'bg-pink-50',
+    'bg-pink-100',
+    'bg-pink-200',
+    'bg-pink-300',
+    'bg-pink-400',
+    'bg-pink-500',
+    'bg-pink-600',
+    'bg-pink-700',
+    'bg-pink-800',
+    'bg-pink-900',
   ];
 };
 
@@ -192,10 +195,10 @@ export const SampleColors = (props) => {
   const colors = tw_colors();
   return (
     <>
-      <div className="grid grid-cols-10">
+      <div className='grid grid-cols-10'>
         {colors.map((color, index) => {
           return (
-            <div key={index} className="h-8 rounded-xl">
+            <div key={index} className='h-8 rounded-xl'>
               <SampleItem onCopy={onCopy} className={color} />
             </div>
           );
@@ -207,14 +210,14 @@ export const SampleColors = (props) => {
 
 export const SampleFontFamilies = (props) => {
   const { onCopy } = props;
-  const FontFamilies = ["font-sans", "font-serif", "font-mono"];
+  const FontFamilies = ['font-sans', 'font-serif', 'font-mono'];
   return (
     <>
-      <div className="grid grid-cols-3">
+      <div className='grid grid-cols-3'>
         {FontFamilies.map((family, index) => {
           return (
-            <div key={index} className="h-8 text-center text-xl rounded-xl">
-              <SampleItem onCopy={onCopy} className={family} title="tailwind" />
+            <div key={index} className='h-8 text-center text-xl rounded-xl'>
+              <SampleItem onCopy={onCopy} className={family} title='tailwind' />
             </div>
           );
         })}
@@ -227,15 +230,15 @@ export const SampleFontStyles = (props) => {
   const { styleItems, optionStyle, baseStyle, onCopy } = props;
   return (
     <>
-      <div className={cc([optionStyle, "grid grid-cols-3"])}>
+      <div className={cc([optionStyle, 'grid grid-cols-3'])}>
         {styleItems.map((item, index) => {
           return (
-            <div key={index} className="text-center rounded-xl">
+            <div key={index} className='text-center rounded-xl'>
               <SampleItem
                 onCopy={onCopy}
                 className={item}
                 optionStyle={baseStyle}
-                title={item.replace("text-", "").replace("font-", "")}
+                title={item.replace('text-', '').replace('font-', '')}
               />
             </div>
           );
@@ -249,10 +252,10 @@ export const SampleInlineStyles = (props) => {
   const { styleItems, optionStyle, baseStyle, onCopy } = props;
   return (
     <>
-      <div className={cc([optionStyle, "grid grid-cols-3"])}>
+      <div className={cc([optionStyle, 'grid grid-cols-3'])}>
         {styleItems.map((item, index) => {
           return (
-            <div key={index} className="text-center rounded-xl">
+            <div key={index} className='text-center rounded-xl'>
               <SampleInlineItem
                 onCopy={onCopy}
                 className={item}
--- pages/index.js
+++ pages/index.js
@@ -1,74 +1,201 @@
-import React, { useState } from "react";
-import Head from "next/head";
-import Nav from "../components/nav";
-import { SampleBox, SampleBoxs, SampleInlineStyles,SampleItem,SampleColors,SampleFontFamilies,SampleFontStyles } from "../components/parts/sample";
-import { PageLink } from "../components/links/link";
-import Link from "next/link";
-import { Popup } from "../components/popup/pop";
+import React, { useState } from 'react';
+import Head from 'next/head';
+import Nav from '../components/nav';
+import {
+  SampleBox,
+  SampleBoxs,
+  SampleInlineStyles,
+  SampleItem,
+  SampleColors,
+  SampleFontFamilies,
+  SampleFontStyles,
+} from '../components/parts/sample';
+import { PageLink } from '../components/links/link';
+import Link from 'next/link';
+import { Popup } from '../components/popup/pop';
 
 const Home = () => {
-  const [comment,setComment] = useState('');
+  const [comment, setComment] = useState('');
   const handleCopy = (text) => {
     setComment(text);
   };
   const links = [
-    {href:"https://tailwindcss.com/",title:"Tailwind.css"},
-    {href:"https://play.tailwindcss.com/f91h03y87s",title:"Tailwind Play"},
-    {href:"https://umeshmk.github.io/Tailwindcss-cheatsheet/",title:"Tailwindcss cheatsheet v-2.0"},
-    {href:"https://zenn.dev/junki555/articles/45614f64695b77a2ae78",title:"TailwindCSS で モバイル縦画面表示時のみ消える要素を作る"},
-    {href:"https://tailwindcomponents.com/cheatsheet/",title:"Tailwind Components cheatsheet"},
-    {href:"https://nerdcave.com/tailwind-cheat-sheet",title:"Tailwind-Cheat-Sheet"},
-    {href:"https://tailblocks.cc/",title:"Tailblocks"},
-    {href:"https://tailwindtemplates.io/",title:"Tailwind Templates"},
-    {href:"https://daisyui.com/",title:"Daisy UI"},
-    {href:"https://devdojo.com/tailwindcss/buttons",title:"TailwindCSS Buttons"},
-    {href:"https://www.creative-tim.com/learning-lab/tailwind-starter-kit/presentation",title:"TAILWIND STARTER KIT"},
-    {href:"https://marketplace.visualstudio.com/items?itemName=bradlc.vscode-tailwindcss",title:"Tailwind CSS IntelliSense"},
-    {href:"https://chakra-ui.com/",title:"Chakra UI"},
-    {href:"https://headlessui.dev/",title:"headless UI"},
-  ]
+    { href: 'https://tailwindcss.com/', title: 'Tailwind.css' },
+    { href: 'https://play.tailwindcss.com/f91h03y87s', title: 'Tailwind Play' },
+    {
+      href: 'https://umeshmk.github.io/Tailwindcss-cheatsheet/',
+      title: 'Tailwindcss cheatsheet v-2.0',
+    },
+    {
+      href: 'https://zenn.dev/junki555/articles/45614f64695b77a2ae78',
+      title: 'TailwindCSS で モバイル縦画面表示時のみ消える要素を作る',
+    },
+    {
+      href: 'https://tailwindcomponents.com/cheatsheet/',
+      title: 'Tailwind Components cheatsheet',
+    },
+    {
+      href: 'https://nerdcave.com/tailwind-cheat-sheet',
+      title: 'Tailwind-Cheat-Sheet',
+    },
+    { href: 'https://tailblocks.cc/', title: 'Tailblocks' },
+    { href: 'https://tailwindtemplates.io/', title: 'Tailwind Templates' },
+    { href: 'https://daisyui.com/', title: 'Daisy UI' },
+    {
+      href: 'https://devdojo.com/tailwindcss/buttons',
+      title: 'TailwindCSS Buttons',
+    },
+    {
+      href: 'https://www.creative-tim.com/learning-lab/tailwind-starter-kit/presentation',
+      title: 'TAILWIND STARTER KIT',
+    },
+    {
+      href: 'https://marketplace.visualstudio.com/items?itemName=bradlc.vscode-tailwindcss',
+      title: 'Tailwind CSS IntelliSense',
+    },
+    { href: 'https://chakra-ui.com/', title: 'Chakra UI' },
+    { href: 'https://headlessui.dev/', title: 'headless UI' },
+  ];
   return (
-    <div className="container max-w-screen-md mx-auto text-sans">
+    <div className='container max-w-screen-md mx-auto text-sans'>
       <Head>
         <title>Tailwind CC</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <div className="w-full text-center text-2xl font-bold">Tailwind CC</div>
-      <div className="w-full text-gray-900 p-4">
-        <div className="text-lg font-bold pt-4">Box</div>
-        <div className="flex flex-row flex-wrap justify-center items-center">
-          <SampleBox onCopy={handleCopy} className="container mx-auto" dammyStyle="flex justify-center items-center px-5" />
-          <SampleBox onCopy={handleCopy} className="min-h-full flex justify-center items-center" fitNone={true} />
-          <SampleBox onCopy={handleCopy} className="flex justify-center items-center" />
-          <SampleBoxs onCopy={handleCopy} className="flex justify-center items-center" />
-          <SampleBoxs onCopy={handleCopy} className="flex flex-col" />
-          <SampleBoxs onCopy={handleCopy} className="flex flex-row" />
-          <SampleBoxs onCopy={handleCopy} className="grid grid-cols-1" />
-          <SampleBoxs onCopy={handleCopy} className="grid grid-cols-2" />
-          <SampleBoxs onCopy={handleCopy} className="grid grid-cols-3" />
-          <SampleBoxs onCopy={handleCopy} className="grid grid-cols-4" />
-          <SampleBoxs onCopy={handleCopy} className="grid grid-cols-5" />
+      <div className='w-full text-center text-2xl font-bold'>Tailwind CC</div>
+      <div className='w-full text-gray-900 p-4'>
+        <div>
+          <div className='text-lg font-bold pt-4'>Box</div>
+          <div className='flex flex-row flex-wrap justify-center items-center'>
+            <SampleBox
+              onCopy={handleCopy}
+              className='container mx-auto'
+              dammyStyle='flex justify-center items-center px-5'
+            />
+            <SampleBox
+              onCopy={handleCopy}
+              className='flex justify-center items-center content-center'
+              size='small'
+            />
+            <SampleBox
+              onCopy={handleCopy}
+              className='flex justify-center items-center'
+            />
+            <SampleBoxs
+              onCopy={handleCopy}
+              className='flex justify-center items-center'
+            />
+            <SampleBoxs onCopy={handleCopy} className='flex flex-col' />
+            <SampleBoxs onCopy={handleCopy} className='flex flex-row' />
+            <SampleBoxs onCopy={handleCopy} className='grid grid-cols-1' />
+            <SampleBoxs onCopy={handleCopy} className='grid grid-cols-2' />
+            <SampleBoxs onCopy={handleCopy} className='grid grid-cols-3' />
+            <SampleBoxs onCopy={handleCopy} className='grid grid-cols-4' />
+            <SampleBoxs onCopy={handleCopy} className='grid grid-cols-5' />
+          </div>
         </div>
-        <div className="text-lg font-bold pt-4">Color</div>
-        <SampleColors onCopy={handleCopy} />
-        <div className="text-lg font-bold pt-4">Font Family</div>
-        <SampleFontFamilies onCopy={handleCopy} />
-        <div className="text-lg font-bold pt-4">Font Size</div>
-        <SampleFontStyles onCopy={handleCopy} styleItems={['text-xs','text-sm','text-base','text-lg','text-xl','text-2xl','text-3xl','text-4xl','text-5xl','text-6xl']} optionStyle="items-center h-full" />
-        <div className="text-lg font-bold pt-4">Font Weight</div>
-        <SampleFontStyles onCopy={handleCopy} styleItems={['font-hairline','font-thin','font-light','font-normal','font-medium','font-semibold','font-bold','font-extrabold','font-black']} optionStyle="items-center h-full" />
-        <div className="text-lg font-bold pt-4">Rounded</div>
-        <SampleFontStyles onCopy={handleCopy} styleItems={['rounded-none','rounded-sm','rounded','rounded-md','rounded-lg','rounded-xl','rounded-2xl','rounded-3xl','rounded-full']} baseStyle="p-2" />
-        <div className="text-lg font-bold pt-4">Text Align</div>
-        <SampleFontStyles onCopy={handleCopy} styleItems={['text-left','text-right','text-center','text-justify']} baseStyle="p-2" />
-        <div className="text-lg font-bold pt-4">Shadow</div>
-        <SampleFontStyles onCopy={handleCopy} styleItems={['shadow-inner','shadow-sm','shadow','shadow-md','shadow-lg','shadow-xl','shadow-2xl']} baseStyle="p-2" />
-        <div className="text-lg font-bold pt-4">Links</div>
-        <div className="pl-4 grid grid-cols-3">
-          {links.map((link,index)=>{
-            return (<PageLink key={index} href={link.href} title={Link.title} />)
-          })}
+        <div>
+          <div className='text-lg font-bold pt-4'>Color</div>
+          <SampleColors onCopy={handleCopy} />
+        </div>
+        <div>
+          <div className='text-lg font-bold pt-4'>Font Family</div>
+          <SampleFontFamilies onCopy={handleCopy} />
+        </div>
+        <div>
+          <div className='text-lg font-bold pt-4'>Font Size</div>
+          <SampleFontStyles
+            onCopy={handleCopy}
+            styleItems={[
+              'text-xs',
+              'text-sm',
+              'text-base',
+              'text-lg',
+              'text-xl',
+              'text-2xl',
+              'text-3xl',
+              'text-4xl',
+              'text-5xl',
+              'text-6xl',
+            ]}
+            optionStyle='items-center h-full'
+          />
+        </div>
+        <div>
+          <div className='text-lg font-bold pt-4'>Font Weight</div>
+          <SampleFontStyles
+            onCopy={handleCopy}
+            styleItems={[
+              'font-hairline',
+              'font-thin',
+              'font-light',
+              'font-normal',
+              'font-medium',
+              'font-semibold',
+              'font-bold',
+              'font-extrabold',
+              'font-black',
+            ]}
+            optionStyle='items-center h-full'
+          />
+        </div>
+        <div>
+          <div className='text-lg font-bold pt-4'>Rounded</div>
+          <SampleFontStyles
+            onCopy={handleCopy}
+            styleItems={[
+              'rounded-none',
+              'rounded-sm',
+              'rounded',
+              'rounded-md',
+              'rounded-lg',
+              'rounded-xl',
+              'rounded-2xl',
+              'rounded-3xl',
+              'rounded-full',
+            ]}
+            baseStyle='p-2'
+          />
+        </div>
+        <div>
+          <div className='text-lg font-bold pt-4'>Text Align</div>
+          <SampleFontStyles
+            onCopy={handleCopy}
+            styleItems={[
+              'text-left',
+              'text-right',
+              'text-center',
+              'text-justify',
+            ]}
+            baseStyle='p-2'
+          />
+        </div>
+        <div>
+          <div className='text-lg font-bold pt-4'>Shadow</div>
+          <SampleFontStyles
+            onCopy={handleCopy}
+            styleItems={[
+              'shadow-inner',
+              'shadow-sm',
+              'shadow',
+              'shadow-md',
+              'shadow-lg',
+              'shadow-xl',
+              'shadow-2xl',
+            ]}
+            baseStyle='p-2'
+          />
+        </div>
+        <div>
+          <div className='text-lg font-bold pt-4'>Links</div>
+          <div className='pl-4 grid grid-cols-3'>
+            {links.map((link, index) => {
+              return (
+                <PageLink key={index} href={link.href} title={Link.title} />
+              );
+            })}
+          </div>
         </div>
         <Popup comment={comment} />
       </div>
@@ -78,7 +205,7 @@ const Home = () => {
 
 export async function getStaticProps() {
   return {
-    props: {}
+    props: {},
   };
 }
 

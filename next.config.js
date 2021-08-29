0f946932486205a876e236cdc9af862023cf9004
cti1650 <cti1650@gmail.com>
2021-08-29 14:31:50 +0900

    🐛 Tailwindを再インストール

--- next.config.js
+++ next.config.js
@@ -1,2 +1,8 @@
-const withCSS = require("@zeit/next-css");
-module.exports = withCSS({});
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+};
+
+module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles.css';
+import 'tailwindcss/tailwind.css';
 
 import React from 'react';
 import App from 'next/app';

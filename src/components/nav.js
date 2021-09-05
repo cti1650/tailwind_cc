2a664894749fabedbfcc7a4ebce4ec2d098778d5
cti1650 <cti1650@gmail.com>
2021-09-05 23:40:10 +0900

    🐛 エラー箇所修正

---
+++ src/components/nav.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from 'next/link';
+
+const links = [
+  { href: 'https://zeit.co/now', label: 'ZEIT' },
+  { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
+].map((link) => {
+  link.key = `nav-link-${link.href}-${link.label}`;
+  return link;
+});
+
+const Nav = () => (
+  <nav className='text-center'>
+    <ul className='flex justify-between px-4 my-4 py-1'>
+      <li className='flex px-2 py-1'>
+        <Link href='/'>
+          <a className='text-blue-500	no-underline text-sm'>Home</a>
+        </Link>
+      </li>
+      {links.map(({ key, href, label }) => (
+        <li key={key} className='flex px-2 py-1'>
+          <a className='text-blue-500	no-underline text-sm' href={href}>
+            {label}
+          </a>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
+
+export default Nav;

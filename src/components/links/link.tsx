bf55f3d91ce2ffc5359c80d952f0f82e3c0589f8
cti1650 <cti1650@gmail.com>
2021-11-02 11:06:42 +0900

    💄 Linksにタイトル表示

--- src/components/links/link.tsx
+++ src/components/links/link.tsx
@@ -11,19 +11,22 @@ export const PageLink = (props) => {
   };
   return (
     <>
-      <div className='w-full h-full'>
-        <a
-          href={href}
-          title={title}
-          onClick={ClickEvent}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <img
-            src={'https://capture.heartrails.com/200x200/cool?' + href}
-            alt='linkimg'
-          />
-        </a>
+      <div className='w-full h-full flex justify-center items-center'>
+        <div className='relative'>
+          <a
+            href={href}
+            title={title}
+            onClick={ClickEvent}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img
+              src={'https://capture.heartrails.com/200x200/cool/delay=3?' + href}
+              alt='linkimg'
+            />
+          </a>
+          {title && (<div className="absolute bottom-0 left-0 px-3 pb-5"><span className="bg-white whitespace-pre-wrap px-2 py-1 rounded text-xs">{title}</span></div>)}
+        </div>
       </div>
     </>
   );

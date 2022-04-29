12b35fcdbcccfc67182b54149166e38301f2bbab
cti1650 <cti1650@gmail.com>
2022-04-29 19:54:27 +0000

    fix: スマホの表示崩れに対応

--- src/components/layout/Section.tsx
+++ src/components/layout/Section.tsx
@@ -8,7 +8,7 @@ type Props = {
 export const Section: VFC<Props> = ({ title, children }) => {
   return (
     <div className='w-full flex flex-col space-y-2 mb-6'>
-      <div className='sticky top-0 h-[40px] bg-white opacity-75 z-10 text-lg font-bold pt-4'>
+      <div className='text-lg font-bold pt-4'>
         {title}
       </div>
       {children}
